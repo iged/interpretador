78ae0bec411f3db12ebb478cba6289ef68153f1a
Tiago Neves <tiagodvneves@yahoo.com.br>
2012-04-10 18:58:33 -0300

    commit

--- src/br/ufpb/iged/interpretador/bytecodeassembler/asm/BytecodeAssembler.java
+++ src/br/ufpb/iged/interpretador/bytecodeassembler/asm/BytecodeAssembler.java
@@ -1,15 +1,26 @@
 package br.ufpb.iged.interpretador.bytecodeassembler.asm;
 
-import org.antlr.runtime.RecognizerSharedState;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.antlr.runtime.TokenStream;
 
 import br.ufpb.iged.interpretador.bytecodeassembler.parser.AssemblerParser;
 
 public class BytecodeAssembler extends AssemblerParser{
+	
+	protected Map<String,Integer> opcodesInstrucoes =
+	        new HashMap<String,Integer>();
 
-	public BytecodeAssembler(TokenStream lexer, RecognizerSharedState state) {
+	public BytecodeAssembler(TokenStream lexer, Definicao.Instrucao[] instrucoes) {
 		
 		super(lexer);
+		
+		for (int i = 1; i < instrucoes.length; i++) {
+			
+			opcodesInstrucoes.put(instrucoes[i].nome.toLowerCase(), i);
+			
+		}
 		
 	}
 
