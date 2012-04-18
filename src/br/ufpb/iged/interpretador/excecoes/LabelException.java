10722108836dd8024539c6282471b9e3e7674ad9
Tiago Neves <tiagodvneves@yahoo.com.br>
2012-04-18 17:44:51 -0300

    commit

--- src/br/ufpb/iged/interpretador/excecoes/LabelException.java
+++ src/br/ufpb/iged/interpretador/excecoes/LabelException.java
@@ -1,22 +1,22 @@
 package br.ufpb.iged.interpretador.excecoes;
 
-public class LabelException extends Exception{ 
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -8079795783286968296L;
+public class LabelException extends Exception{
 
-	public LabelException() {
-		
-		super();
-		
-	}
-	
-	public LabelException(String msg) {
-		
-		super(msg);
-		
-	}
+/**
+*
+*/
+private static final long serialVersionUID = -8079795783286968296L;
+
+public LabelException() {
+
+super();
+
+}
+
+public LabelException(String msg) {
+
+super(msg);
+
+}
 
 }
